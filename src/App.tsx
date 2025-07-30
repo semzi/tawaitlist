0e219b1bb819005b53f7c1e4635341d0d3f3b223
Maxi <adekanyesemilore91@gmail.com>
2025-07-30 23:11:25 +0100

    feat: Add waitlist functionality and social sharing features
    - Updated package.json to include new dependencies: @heroicons/react, axios, and react-toastify.
    - Added new images for social sharing (facebook.png, whatsapp.png, x.png).
    - Enhanced App component to manage waitlist submissions with success feedback and social sharing options.
    - Introduced Footer component for improved layout and branding.
    - Updated CSS for better visual effects.
    - Configured Vite to proxy API requests to the backend service.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
 import React, { useState, useEffect } from "react";
 import "./index.css";
 import FormInput from "./FormInput";
-import "./fade.js"
-import { Book, Calendar1Icon, Lightbulb, ShipWheel } from "lucide-react";
+import "./fade.js";
+import {
+  BotMessageSquare,
+  Calendar1Icon,
+  CopyIcon,
+  Newspaper,
+  Trophy,
+} from "lucide-react";
+import FooterComp from "./Footer";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import axios from "axios";
+
+const baseUrl = "https://tikianaly-service-backend.onrender.com";
+const endpoint = "/api/v1/waitlist/join-waitlist";
 
 // Custom hook to fetch random profile pictures
 const useRandomProfilePictures = (count: number) => {
@@ -38,7 +51,7 @@ const useRandomProfilePictures = (count: number) => {
 function App(): React.JSX.Element {
   // const [email, setEmail] = useState('')
   const { profilePictures, loading } = useRandomProfilePictures(4);
-
+  const [success, setSuccess] = useState(false);
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
     // Handle form submission
@@ -47,6 +60,7 @@ function App(): React.JSX.Element {
 
   return (
     <div className="min-h-screen bg-white">
+      <ToastContainer />
       {/* Header */}
       <header className="text-center parallax-content py-8">
         <div className="page-padding-x">
@@ -55,85 +69,205 @@ function App(): React.JSX.Element {
             alt="logo"
             className="h-20 max-w-lg mx-auto"
           />
-          <div className="flex bg-transparent w-fit mx-auto  text-blue-600 my-auto px-5 py-2 items-center rounded-full font-medium border-2 border-blue-600 mt-4">
+          <div className="flex backdrop-blur-lg bg-blue-50 w-fit mx-auto text-sm  text-blue-600 my-auto px-5 py-2 items-center rounded-full font-medium border-2 border-blue-600 mt-4">
             <Calendar1Icon className="inline-block h-5 mr-1" />
             <p>Available in Early 2026</p>
           </div>
         </div>
       </header>
 
-      {/* Hero Section */}
-      <section className=" pb-12">
-        <div className="page-padding-x ">
-          <h2 className="md:text-4xl text-3xl max-w-lg mx-auto text-center font-bold text-gray-800 mb-8 animate-glass-shine">
-            Get early access to a Game-Changing Platform
-          </h2>
-          <p className="text-gray-600 text-md mb-10 max-w-2xl mx-auto text-center">
-            TikiAnaly is transforming how fans, scouts, analysts, and athletes
-            interact with sports data from the grassroots to the global stage.
-            Be among the first to experience AI-powered insights, live local
-            coverage, and a platform built to celebrate every sport, every club,
-            everywhere.
+      {success && (
+        <div className="flex flex-col items-center page-padding-x mx-auto">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="#0fb20f"
+            className="w-30 mb-5 animate-bounce"
+          >
+            <path
+              fillRule="evenodd"
+              d="M8.603 3.799A4.49 4.49 0 0 1 12 2.25c1.357 0 2.573.6 3.397 1.549a4.49 4.49 0 0 1 3.498 1.307 4.491 4.491 0 0 1 1.307 3.497A4.49 4.49 0 0 1 21.75 12a4.49 4.49 0 0 1-1.549 3.397 4.491 4.491 0 0 1-1.307 3.497 4.491 4.491 0 0 1-3.497 1.307A4.49 4.49 0 0 1 12 21.75a4.49 4.49 0 0 1-3.397-1.549 4.49 4.49 0 0 1-3.498-1.306 4.491 4.491 0 0 1-1.307-3.498A4.49 4.49 0 0 1 2.25 12c0-1.357.6-2.573 1.549-3.397a4.49 4.49 0 0 1 1.307-3.497 4.49 4.49 0 0 1 3.497-1.307Zm7.007 6.387a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z"
+              clipRule="evenodd"
+            />
+          </svg>
+          <h3 className="text-4xl transition-all font-bold text-gray-800 text-center mb-6">
+            You’re on the Waitlist
+          </h3>
+          <p className="text-gray-600 text-md mb-5 max-w-2xl text-center">
+            You’re in! Get ready to explore the future of sports with AI-powered
+            insights and live grassroots action. We will notify you when we
+            launch. Stay tuned!
           </p>
-
-          {/* Waitlist Form */}
-          <form onSubmit={handleSubmit} className="max-w-lg mx-auto mb-3">
-            <div className="mb-4">
-              <FormInput
-                type="email"
-                id="email"
-                label="Email Address"
-                placeholder="example@gmail.com"
-                icon="/Vector.png"
-                required
-              />
-            </div>
-            <button
-              type="submit"
-              className="w-full bg-blue-600 text-white font-bold py-2 px-6 rounded-sm hover:bg-blue-700 transition-colors"
+          
+          <div
+            className="bg-gray-200 text-gray-500 w-[100%] flex justify-between rounded md:w-xl p-4 cursor-pointer"
+            onClick={() => {
+              navigator.clipboard.writeText("https://www.tikianaly.com/");
+              toast.success("Link copied!");
+            }}
+            title="Copy link"
+          >
+            <p className="text-sm text-center select-none">
+              https://www.tikianaly.com/
+            </p>
+            <CopyIcon className="h-4" />
+          </div>
+          <div className="flex mt-8 gap-3">
+            <div
+              className="flex items-center gap-2 bg-[#0ECD4C] px-4 py-2 rounded w-full sm:w-auto"
+              onClick={() => {
+                navigator.clipboard.writeText(
+                  "🚀 Just joined the TikiAnaly waitlist – AI-powered grassroots sports insights! 🔥\nJoin here: https://www.tikianaly.com/"
+                );
+                toast.success("Text Copied");
+              }}
             >
-              JOIN THE WAITLIST
-            </button>
-          </form>
-
-          {/* Social Proof */}
-          <div className="flex items-center max-w-lg mx-auto gap-2 text-gray-600">
-            <div className="flex -space-x-1">
-              {loading ? (
-                <div className="w-6 h-6 bg-gray-300 rounded-full border-2 border-white"></div>
-              ) : (
-                profilePictures.map((picture, index) => (
-                  <img
-                    key={index}
-                    src={picture}
-                    alt={`Profile ${index + 1}`}
-                    className="w-6 h-6 rounded-full border-2 border-white"
-                  />
-                ))
-              )}
+              <img src="/whatsapp.png" alt="Whatsapp" className="w-5 h-5" />
+              <p className="text-white text-sm">Share</p>
             </div>
-            <span className="text-sm">Join 234+ others in the waitlist</span>
+            <div
+              className="flex items-center gap-2 bg-[#0C0C0C] px-4 py-2 rounded w-full sm:w-auto"
+              onClick={() => {
+                navigator.clipboard.writeText(
+                  "🚀 Just joined the TikiAnaly waitlist – AI-powered grassroots sports insights! 🔥\nJoin here: https://www.tikianaly.com/"
+                );
+                toast.success("Text Copied");
+              }}
+            >
+              <img src="/x.png" alt="X" className="w-5 h-5" />
+              <p className="text-white text-sm">Share</p>
+            </div>
+            <div
+              className="flex items-center gap-2 bg-[#3D6AD6] px-4 py-2 rounded w-full sm:w-auto"
+              onClick={() => {
+                navigator.clipboard.writeText(
+                  "🚀 Just joined the TikiAnaly waitlist – AI-powered grassroots sports insights! 🔥\nJoin here: https://www.tikianaly.com/"
+                );
+                toast.success("Text Copied");
+              }}
+            >
+              <img src="/facebook.png" alt="Facebook" className="h-5" />
+              <p className="text-white text-sm">Share</p>
+            </div>
           </div>
         </div>
-      </section>
+      )}
 
-      <img src="/mockup.png" alt="" className="fade-on-scroll opacity-0 transition-none w-100 mx-auto" />
+      {/* Hero Section */}
+      {!success && (
+        <section className=" pb-12">
+          <div className="page-padding-x ">
+            <h2 className="md:text-4xl text-3xl max-w-lg mx-auto text-center pb-3 font-bold text-gray-800 mb-5 animate-glass-shine">
+              Get early access to a Game-Changing Platform
+            </h2>
+            <p className="text-gray-600 text-md mb-10 max-w-2xl mx-auto text-center">
+              TikiAnaly is transforming how fans, scouts, analysts, and athletes
+              interact with sports data from the grassroots to the global stage.
+              Be among the first to experience AI-powered insights, live local
+              coverage, and a platform built to celebrate every sport, every
+              club, everywhere.
+            </p>
+
+            {/* Waitlist Form */}
+            <form
+              onSubmit={async (e) => {
+                e.preventDefault();
+                const form = e.target as HTMLFormElement;
+                const emailInput = form.elements.namedItem(
+                  "email"
+                ) as HTMLInputElement;
+                const email = emailInput.value;
+
+                axios
+                  .post(`${endpoint}`, {
+                    email: email,
+                  })
+                  .then((response) => {
+                    toast.success(
+                      response?.data?.message ||
+                        "Successfully joined the waitlist!"
+                    );
+                    form.reset();
+                    setSuccess(true);
+                  })
+                  .catch((error) => {
+                    if (error?.response?.status === 409) {
+                      toast.info(
+                        error?.response?.data?.message ||
+                          "You’re already on the waitlist!"
+                      );
+                    } else {
+                      toast.error(
+                        error?.response?.data?.message ||
+                          "Failed to join the waitlist. Please try again."
+                      );
+                    }
+                  });
+              }}
+              className="max-w-lg mx-auto mb-3"
+            >
+              <div className="mb-4">
+                <FormInput
+                  type="email"
+                  id="email"
+                  name="email"
+                  label="Email Address"
+                  placeholder="example@gmail.com"
+                  icon="/Vector.png"
+                  required
+                />
+              </div>
+              <button
+                type="submit"
+                className="w-full bg-blue-600 text-white font-bold py-2 px-6 rounded-sm hover:bg-blue-700 transition-colors"
+              >
+                JOIN THE WAITLIST
+              </button>
+            </form>
+
+            {/* Social Proof */}
+            <div className="flex items-center max-w-lg mx-auto gap-2 text-gray-600">
+              <div className="flex -space-x-1">
+                {loading ? (
+                  <div className="w-6 h-6 bg-gray-300 rounded-full border-2 border-white"></div>
+                ) : (
+                  profilePictures.map((picture, index) => (
+                    <img
+                      key={index}
+                      src={picture}
+                      alt={`Profile ${index + 1}`}
+                      className="w-6 h-6 rounded-full border-2 border-white"
+                    />
+                  ))
+                )}
+              </div>
+              <span className="text-sm">Join 234+ others in the waitlist</span>
+            </div>
+          </div>
+        </section>
+      )}
+
+      <img
+        src="/mockup.png"
+        alt=""
+        className="fade-on-scroll opacity-0 transition-none w-100 mx-auto"
+      />
       {/* Features Section */}
       <section className="py-16 bg-gray-50">
         <div className="page-padding-x">
           <h3 className="text-3xl fade-on-scroll opacity-0 transition-none font-bold text-gray-800 text-center mb-12">
             Features Built With You! <br /> Yes, You in Mind
           </h3>
-          <div className="md:flex gap-4 ">
+          <div className="md:flex gap-2 ">
             {/* AI Chat Feature */}
-            <div className="flex fade-on-scroll opacity-0 transition-none flex-1/3 shadow-2xl rounded-xl bg-white p-5 flex-col">
-              <div className="flex gap-2 mb-4">
-                <Lightbulb className="h-8 w-8" />
-                <h4 className="text-lg font-semibold text-gray-800">
-                  AI Chat — Your Personal Sports Analyst
+            <div className="flex border hover:-translate-y-3 transition-all hover:scale-100 scale-95 fade-on-scroll opacity-0 transition-none flex-1/3 shadow-2xl rounded-xl bg-white p-5 flex-col">
+              <div className="flex flex-col items-center gap-2 mb-4">
+                <BotMessageSquare className="h-8 w-8 text-[#ff5414]" />
+                <h4 className="text-lg text-center font-semibold text-gray-800">
+                  AI Chat <br /> Your Personal Sports Analyst
                 </h4>
               </div>
-              <p className="text-gray-600 text-sm">
+              <p className="text-gray-600 text-center text-sm">
                 Ask it anything from “Who scored in the local derby last week?”
                 to “What’s the best formation for 7-a-side?” This isn’t just a
                 chatbot; it’s your sport-savvy bestie with endless banter,
@@ -142,14 +276,14 @@ function App(): React.JSX.Element {
             </div>
 
             {/* Sport Varieties Feature */}
-            <div className="flex  fade-on-scroll opacity-0 transition-none shadow-2xl flex-1/3 bg-[#0056D2] my-6 md:my-1 rounded-xl  p-5 flex-col">
-              <div className="flex text-white gap-2 mb-4">
-                <ShipWheel className="h-8 w-8" />
-                <h4 className="text-lg font-semibold">
+            <div className="flex border hover:-translate-y-3 transition-all hover:scale-100 scale-95 fade-on-scroll opacity-0 transition-none flex-1/3 shadow-2xl rounded-xl bg-white p-5 flex-col">
+              <div className="flex flex-col items-center gap-2 mb-4">
+                <Trophy className="h-8 w-8 text-[#ff5414]" />
+                <h4 className="text-lg text-center font-semibold text-gray-800">
                   Every Sport, Every Street, Every Stat
                 </h4>
               </div>
-              <p className="text-white text-sm">
+              <p className="text-gray-600 text-center text-sm">
                 We cover more than just the mainstream. Whether it’s streetball,
                 futsal, volleyball, or table tennis finals in your neighborhood;
                 We see you! <br /> TikiAnaly celebrates the unsung heroes, wild
@@ -158,14 +292,14 @@ function App(): React.JSX.Element {
             </div>
 
             {/* Top News Feature */}
-            <div className="flex fade-on-scroll opacity-0 transition-none flex-1/3 shadow-2xl rounded-xl bg-white p-5 flex-col">
-              <div className="flex gap-2 mb-4">
-                <Book className="h-8 w-8" />
-                <h4 className="text-lg font-semibold text-gray-800">
+            <div className="flex border hover:-translate-y-3 transition-all hover:scale-100 scale-95 fade-on-scroll opacity-0 transition-none flex-1/3 shadow-2xl rounded-xl bg-white p-5 flex-col">
+              <div className="flex flex-col items-center gap-2 mb-4">
+                <Newspaper className="h-8 w-8 text-[#ff5414]" />
+                <h4 className="text-lg text-center font-semibold text-gray-800">
                   Top News
                 </h4>
               </div>
-              <p className="text-gray-600 text-sm">
+              <p className="text-gray-600 text-center text-sm">
                 From hyper-local match updates to big club scoops, our news feed
                 is built for the community. Smart, fun, and straight to the
                 point like your favourite group chat, but with fewer arguments
@@ -177,13 +311,7 @@ function App(): React.JSX.Element {
       </section>
 
       {/* Footer */}
-      <footer className="py-8 text-center">
-        <div className="page-padding-x">
-          <p className="text-gray-500 text-sm">
-            Copyright © 2025 TikiAnaly. All Rights Reserved.
-          </p>
-        </div>
-      </footer>
+      <FooterComp />
       {/* <script src="/fade.js"></script> */}
     </div>
   );
---
+++ src/Footer.tsx
@@ -0,0 +1,47 @@
+// import  footerLinks  from '/data/footerLink'
+const footerLinks = [
+  { label: "About Us", href: "#" }
+];
+
+export const FooterComp = () => {
+  return (
+    <div className="page-padding-x py-15 bg-brand-primary  text-white">
+      <div className="flex-col gap-y-15 lg:flex-row flex justify-between ">
+        <div
+          className="flex flex-col gap-7
+         max-w-150"
+        >
+          <h1 className="text-xl md:text-2xl font-bold">TikiAnaly</h1>
+          <p className="">
+            TikiAnaly is transforming how fans, scouts, analysts, and athletes
+            interact with sports data from the grassroots to the global stage.
+            Be among the first to experience AI-powered insights, live local
+            coverage, and a platform built to celebrate every sport, every club,
+            everywhere.
+          </p>
+          <div className="flex gap-7">
+            <img src="/facebook.png" className=" h-8" alt="" />
+            <img src="/x.png" className=" h-8" alt="" />
+          </div>
+        </div>
+        <div className="links sz-4">
+          <div className="grid grid-cols-2 gap-x-30 sm:grid-cols-2 gap-4">
+            {footerLinks.map((link, index) => (
+              <a
+                key={index}
+                href={link.href}
+                className="hover:text-brand-secondary text-sm"
+              >
+                {link.label}
+              </a>
+            ))}
+          </div>
+        </div>
+      </div>
+      <p className="text-center mb-8 mt-18">
+        Copyright ©2025 Tikianaly / All rights reserved
+      </p>
+    </div>
+  );
+};
+export default FooterComp;
